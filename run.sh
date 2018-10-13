72cd0d0b1830c4c6ddd463de4fbd8ae243273cf9
mv-mac <misha6213501@yandex.ru>
2018-10-13 22:55:40 +0300

    file exist? --> overwrite?

--- run.sh
+++ run.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-echo "--------Begin-------"
+echo ""
+echo "--------Start-------"
 
-[ -f run.sh ] #файл существует?
-echo $? #0=да,1=нет
+echo "Enter file name: "
+read filename
 
-echo "$filename"
-echo "--------Done---------"
+[ -f $filename ] #файл существует? $? --> 0=да,1=нет
+
+if [ $? == 0 ]; then
+	echo "File exist! Do you want to overwrite? [y/n]"
+	read flag
+	if [ $flag = "y" ]; then
+		echo "File was overwritte"
+	else
+		echo "File was not overwritte"
+	fi
+else 
+	echo "file not exist"
+fi
+
+
+echo "--------Finish---------"
+echo ""
